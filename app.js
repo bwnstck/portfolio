b02005eebd9cd2ae9084087c001ee25c6733d53c
bwnstk <info@flimmerkiste.xyz>
2020-10-03 15:50:56 +0200

    Add Animations

--- app.js
+++ app.js
@@ -8,4 +8,13 @@ $(".popover-dismiss").popover({
   trigger: "focus",
 });
 
-gsap.to(".avatar", { duration: 2, y: "10vh" });
+// GSAP
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+
+tl.from("h1", { opacity: 0, duration: 1, y: -150 })
+  .from("p.lead", { opacity: 0, duration: 1, y: -150 })
+  .from(".navbar-brand", {
+    opacity: 0,
+    duration: 1,
+    x: -50,
+  });
